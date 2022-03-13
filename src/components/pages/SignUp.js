7a7598ead7e1ce6bfbb77aadd4205ee477047c86
WhiteRev <35569713+WhiteRev@users.noreply.github.com>
2022-03-13 19:25:37 +0100

    Add files via upload

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
+
+function Products() {
+  return (
+    <>
+      <div>
+      <h1 className='products'>LIKE & SUBSCRIBE</h1>;      
+      <Cards />
+      <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Products;
